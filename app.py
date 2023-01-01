8a5c7192b16ed50df6b627403f7b9b828dd7d778
GeekyMovies <121683432+GeekyMovies@users.noreply.github.com>
2023-01-01 08:18:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-   return 'LazyDeveloper'
+   return 'GeekyMovies'
 
 os.system("git clone https://GeekyMovies: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@github.com/GeekyMovies/LazyPrincessV2 okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
